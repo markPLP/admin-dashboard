dcb11b82e4139b79b9aad285c2c84bc96eabe02e
markPLP <mpclaspinas@gmail.com>
2025-03-12 13:44:21 +0800

    Push local copy

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Overview } from './OverviewPage';
+export { default as ProductsPage } from './ProductsPage';
+export { default as UsersPage } from './UsersPage';
+export { default as SalesPage } from './SalesPage';
+export { default as OrdersPage } from './OrdersPage';
+export { default as AnalyticsPage } from './AnalyticsPage';
+export { default as SettingsPage } from './SettingsPage';
